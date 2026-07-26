9c0de4556b3f29367672aa32043fe32d243548f8
alitz <alitz1382@gmail.com>
2026-07-26 15:40:12 +0330

    chore: add shadcn/ui components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { AppProvider } from "@/providers/app-provider";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "E-Commerce",
@@ -14,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" suppressHydrationWarning>
+    <html lang="fa" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body>
         <AppProvider>{children}</AppProvider>
       </body>
